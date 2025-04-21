ca72a963a65da7f6c20920f4c5f4aef0e12840a0
MissyCanada2019 <terri.bertin57@gmail.com>
2025-04-20 21:20:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -317,3 +317,8 @@ def download_final_pdf():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
