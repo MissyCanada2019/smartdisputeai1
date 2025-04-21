3f3bfcf08cdbf6ec08e96f3d728aa4a87c60d266
MissyCanada2019 <terri.bertin57@gmail.com>
2025-04-21 04:15:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -327,3 +327,8 @@ def form():
   @app.route('/form')
 def form():
     return render_template('submit.html')  
+   import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) 
