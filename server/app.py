92af5c79e07df009f52cd07c730504d2b45d903e
MissyCanada2019 <terri.bertin57@gmail.com>
2025-04-23 20:52:53 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -11,3 +11,9 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smartdispute.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+register_routes(app)
