dee322c78c1e4e1886c4ebc2eccc933147b8f7d8
MissyCanada2019 <terri.bertin57@gmail.com>
2025-04-24 06:26:19 -0400

    Add files via upload

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,1 @@
-python
+# Init file
--- server/models.py
+++ server/models.py
@@ -4,5 +4,5 @@ db = SQLAlchemy()
 
 class Case(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    url = db.Column(db.String(500))
+    title = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(1024), nullable=False)
