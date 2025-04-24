1c2789d2c33a41926632fd5f7768e899d49608be
MissyCanada2019 <terri.bertin57@gmail.com>
2025-04-24 06:19:55 -0400

    Delete server/server/models.py

--- server/server/models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Case(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    url = db.Column(db.String(500))
